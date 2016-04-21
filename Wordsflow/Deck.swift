5f1049fb765b46a7a045e0d0cf1a2798e35d2d16
Dan K <itsdanya@gmail.com>
2016-04-21 23:58:42 +0300

    Create Deck class

---
+++ Wordsflow/Deck.swift
@@ -0,0 +1,21 @@
+//
+//  Deck.swift
+//  Wordsflow
+//
+//  Created by Dan K. on 2016-04-21.
+//  Copyright © 2016 Dan K. All rights reserved.
+//
+
+import Foundation
+
+class Deck {
+    
+    let name: String
+    
+    var cards = [Card]()
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+}
