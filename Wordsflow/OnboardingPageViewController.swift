0c9ce93acee11bebc5d1a1658d0d3c9be9eeef2b
Dan K <itsdanya@gmail.com>
2016-04-28 18:37:12 +0300

    Create view controller for onboarding page

---
+++ Wordsflow/OnboardingPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingPageViewController.swift
+//  Wordsflow
+//
+//  Created by Dan K. on 2016-04-28.
+//  Copyright © 2016 Dan K. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingPageViewController: UIViewController {
+    
+    @IBAction func populate() {
+        print("populate")
+    }
+    
+    @IBAction func registerNotifications() {
+        print("registerNotifications")
+    }
+    
+}
