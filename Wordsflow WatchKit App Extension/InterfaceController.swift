561082ffb48f507e14ff87563040e95956a06227
Dan K <itsdanya@gmail.com>
2016-04-30 23:25:01 +0300

    Add Watch target

---
+++ Wordsflow WatchKit App Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Wordsflow WatchKit App Extension
+//
+//  Created by Dan K. on 2016-04-30.
+//  Copyright © 2016 Dan K. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
